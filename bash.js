60068999d6fdae206b8f315f632273b4e7e64232
Daymis Vargas <daymis.vargas@gmail.com>
2018-01-15 12:59:15 -0500

    cat, head, and tail completed

--- bash.js
+++ bash.js
@@ -2,8 +2,10 @@ var commands = require('./commands');
 
 process.stdin.on('data', function(data) {
     var cmd = data.toString().trim();
+    var splitCmd = cmd.split(' ');
+    var method = splitCmd[0];
 
-    commands[cmd]();
-
-    process.stdout.write('You typed: ' + cmd + '\n');
+    commands[method](splitCmd.slice(1).join(' '));
+    
+    //process.stdout.write('You typed: ' + cmd + '\n');
 });
--- commands.js
+++ commands.js
@@ -1,8 +1,9 @@
 module.exports = {
-    pwd: function() {
-        console.log('Current working directory is ' + process.cwd());    
+    pwd: function(item) {
+        console.log('Current working directory is ' + process.cwd());
+        process.stdout.write("prompt > ");    
     },
-    ls: function () {
+    ls: function (item) {
         var fs = require('fs')
         fs.readdir('.', function(err, files) {
             if (err) throw err;
@@ -12,8 +13,42 @@ module.exports = {
             process.stdout.write("prompt > ");
           });
     },
-    date: function () {
+    date: function (item) {
         console.log('Current date is '+ Date());
+        process.stdout.write("prompt > ");
+    },
+    echo: function(item){
+        console.log(item);
+        process.stdout.write("prompt > ");
+    },
+    cat: function(item){
+        var fs = require('fs');
+
+        fs.readFile(item, function(err, files) {
+            if (err) throw err;
+            process.stdout.write(files.toString());
+          });
+
+          process.stdout.write("prompt > ");
+    },
+    head: function(item){
+        var fs = require('fs');
+        var file = fs.readFileSync(item, 'utf8');
+        var lines = file.split('\n');
+
+        for(var i = 0; i < 5; i++){
+            console.log(lines[i]);
+        }
+    },
+    tail: function(item){
+        var fs = require('fs');
+        var file = fs.readFileSync(item, 'utf8');
+        var lines = file.split('\n');
+        
+        for(var i = lines.length - 5; i < lines.length; i++){
+            console.log(lines[i]);
+        }
+
     }
 }
 
